7428200d1fd44f1faf3f29eef0b205f1c1a3614f
SnO2WMaN <sno2wman.official@gmail.com>
2019-01-24 01:43:16 +0900

    feat: add name, birthday, icon and social

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+	name: 'SnO2WMaN',
+	icon: 'https://www.gravatar.com/avatar/9a828752a7771c3bf43e3bea39d9cf57',
+	birthday: new Date(2001, 6, 22),
+	social: require('./social.json')
+}
