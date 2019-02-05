db4594bdc75cf2a9168a605aa67df42710060c13
SnO2WMaN <sno2wman.official@gmail.com>
2019-02-06 07:52:27 +0900

    fix: fix birthday

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ export const name = 'SnO2WMaN'
 export function icon(option = { s: 512 }) {
 	return gravatar.url(socials.email, option)
 }
-export const birthday = new Date(2001, 6, 22)
+export const birthday = new Date(2001, 5, 22)
 export const age = ager(birthday)
 export { default as socials } from './social'
 export default { name, icon, birthday, age, socials }
