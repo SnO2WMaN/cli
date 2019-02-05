03c03021a918b28f950c8b3b114ae9beef75a3a4
SnO2WMaN <sno2wman.official@gmail.com>
2019-02-06 06:35:36 +0900

    chore: add age

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import gravatar from 'gravatar'
+import ager from 'ager'
 
 import socials from './social'
+
+const birthday = new Date(2001, 6, 22)
 
 export default {
 	name: 'SnO2WMaN',
 	icon(option = { s: 512 }) {
 		return gravatar.url(socials.email, option)
 	},
-	birthday: new Date(2001, 6, 22),
-	socials
+	birthday,
+	socials,
+	age: ager(birthday)
 }
