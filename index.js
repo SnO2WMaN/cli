2cc5a48ad8c6a467b365cc353e55dc1a2397303d
SnO2WMaN <sno2wman.official@gmail.com>
2019-02-06 07:57:12 +0900

    chore: add sex, religion ,location

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ export function icon(option = { s: 512 }) {
 }
 export const birthday = new Date(2001, 5, 22)
 export const age = ager(birthday)
+export const sex = 1
+export const religion = 'Pastafarianism'
+export const location = 'Japan, Nagoya'
 export { default as socials } from './social'
-export default { name, icon, birthday, age, socials }
+export default { name, icon, birthday, age, socials, sex, religion, location }
