b49446e968a24b20e700a155d1830ac78c7d0806
SnO2WMaN <sno2wman.official@gmail.com>
2019-02-09 05:30:11 +0900

    chore: remove package "gravatar"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import gravatar from 'gravatar'
+import md5 from 'md5'
+import queryString from 'query-string'
+
 import ager from 'ager'
 
 import socials from './social'
 
 export const name = 'SnO2WMaN'
 export function icon(option = {}) {
-	return gravatar.url(socials.email, option)
+	return `//www.gravatar.com/avatar/${md5(
+		socials.email
+	)}?${queryString.stringify(option)}`
 }
 export const birthday = new Date(2001, 5, 22)
 export const age = ager(birthday)
