4ca825d63b7f2794c2fa535baef2d85c3921b533
SnO2WMaN <sno2wman.official@gmail.com>
2019-02-10 10:27:01 +0900

    test: remove jest/babel config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	testEnvironment: 'node',
-	collectCoverage: true,
-	coveragePathIgnorePatterns: ['/node_modules/', '/test/fixture'],
-	forceExit: true,
-	transform: {
-		'^.+\\.js$': 'babel-jest'
-	}
-}
