afd9d18dd5ee1e59b1d8f356ae987cce48fbdaf7
SnO2WMaN <sno2wman.official@gmail.com>
2019-02-10 13:30:35 +0900

    test: add test suits

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,6 @@
 import test from 'ava'
+import isObject from 'isobject'
+import axios from 'axios'
 import { resolve } from 'path'
 
 const rootDir = resolve(__dirname, '..')
@@ -6,3 +8,30 @@ const sno2wman = require(resolve(rootDir, './dist/index.js'))
 
 test('name', t => t.is(sno2wman.name, 'SnO2WMaN'))
 test('sex', t => t.is(sno2wman.sex, 1))
+test('icon', async t => {
+	await axios
+		.get(sno2wman.icon({ protocol: 'https' }))
+		.then(() => {
+			t.pass()
+		})
+		.catch(({ status, statusText }) => {
+			t.fail(`HTTP Status: ${status} ${statusText}`)
+		})
+})
+test('socials', t => t.true(isObject(sno2wman.socials)))
+Object.entries(sno2wman.socials)
+	.filter(entry => !['discord', 'email'].includes(entry[0]))
+	.forEach(entry => {
+		const key = entry[0],
+			link = entry[1]
+		test(`socials / ${key}`, async t => {
+			await axios
+				.get(link)
+				.then(() => {
+					t.pass()
+				})
+				.catch(({ status, statusText }) => {
+					t.fail(`HTTP Status: ${status} ${statusText}`)
+				})
+		})
+	})
