e811d3ab0148bc102f186bd363d7d2b601d26fa8
SnO2WMaN <sno2wman.official@gmail.com>
2019-02-10 13:31:18 +0900

    fix: fix wrong link

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
-import md5 from 'md5'
-import queryString from 'query-string'
+import gravatar from 'gravatar'
 import ager from 'ager'
 
-import socials from './social'
-
 export const name = 'SnO2WMaN'
-
-export function icon(option = {}) {
-	return `//www.gravatar.com/avatar/${md5(
-		socials.email
-	)}?${queryString.stringify(option)}`
-}
-
 export const birthday = new Date(2001, 5, 22)
 export const age = ager(birthday)
 export const sex = 1
 export const religion = 'Pastafarianism'
 export const location = 'Japan, Nagoya'
-export { default as socials } from './social'
+export const socials = {
+	twitter: 'https://twitter.com/SnO2WMaN',
+	github: 'https://github.com/SnO2WMaN',
+	spotify: 'https://open.spotify.com/user/sno2wman',
+	qiita: 'https://qiita.com/SnO2WMaN',
+	amazon:
+		'https://www.amazon.co.jp/gp/profile/amzn1.account.AGNQ2CQRCY4BH6NT5KPADJFX4KKA',
+	tumblr: 'https://sno2wman.tumblr.com',
+	steam: 'https://steamcommunity.com/id/SnO2WMaN',
+	discord: 'SnO2WMaN#9459',
+	soundcloud: 'https://soundcloud.com/sno2wman',
+	patreon: 'https://www.patreon.com/SnO2WMaN',
+	email: 'sno2wman.official@gmail.com',
+	website: 'https://sno2wman.graphics',
+	annict: 'https://annict.jp/@SnO2WMaN',
+	hatena: 'https://profile.hatena.ne.jp/SnO2WMaN/'
+}
+export function icon(option = {}) {
+	return gravatar.url(socials.email, option)
+}
 export default {
 	name,
 	icon,
