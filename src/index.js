014a152a159dc2722d06b9a4e8fac9c1df4898a5
SnO2WMaN <sno2wman.official@gmail.com>
2019-02-11 02:53:59 +0900

    docs: add JSDoc

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 import gravatar from 'gravatar'
 import ager from 'ager'
 
+/**
+ * Name
+ * @type {String}
+ */
 export const name = 'SnO2WMaN'
+
+/**
+ * Birthday
+ * @type {Date}
+ */
 export const birthday = new Date(2001, 5, 22)
+
+/**
+ * Age
+ * @type {Number}
+ */
 export const age = ager(birthday)
+
+/**
+ * Sex
+ * @type {1|2|9}
+ */
 export const sex = 1
+
+/**
+ * Religion
+ * @type {String}
+ */
 export const religion = 'Pastafarianism'
+
+/**
+ * Location
+ * @type {String}
+ */
 export const location = 'Japan, Nagoya'
+
+/**
+ * Socials List
+ * @typedef Socials
+ */
 export const socials = {
 	twitter: 'https://twitter.com/SnO2WMaN',
 	github: 'https://github.com/SnO2WMaN',
@@ -24,9 +58,22 @@ export const socials = {
 	annict: 'https://annict.jp/@SnO2WMaN',
 	hatena: 'https://profile.hatena.ne.jp/SnO2WMaN/'
 }
+
+/**
+ * Icon Url
+ *
+ * @param {gravatar.Options} option
+ * @return {String} Icon url
+ */
 export function icon(option = {}) {
 	return gravatar.url(socials.email, option)
 }
+
+/**
+ * SnO2WMaN detail
+ *
+ * @typedef sno2wman
+ */
 export default {
 	name,
 	icon,
